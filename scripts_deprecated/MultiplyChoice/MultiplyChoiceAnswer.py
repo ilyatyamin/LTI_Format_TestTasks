049322b0e286ebaa8c943018718fb93f52605f89
Ilya Tyamin <tyaminilya@gmail.com>
2024-04-02 16:28:50 +0300

    Update repo for presentation

--- scripts_deprecated/MultiplyChoice/MultiplyChoiceAnswer.py
+++
@@ -1,9 +0,0 @@
-class MultiplyChoiceAnswer:
-    """
-    Introduce answer-class for questions with type "multiply choice"
-    """
-    def __init__(self):
-        self.text: dict = {}
-        self.points = None
-        self.is_correct = None
-        self.feedback: dict = {}
--- scripts_deprecated/main.py
+++
@@ -1,3 +0,0 @@
-import django
-
-print(django.get_version())
